292c4ae09913bc6240dba64d88ba0aa8e3dba22b
EC2 Default User <ec2-user@ip-172-31-55-219.ec2.internal>
2021-11-13 18:39:43 +0000

    fitst commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,68 @@
+class BooksController < ApplicationController
+  
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+       redirect_to book_path(@book), notice: 'Book was successfully created.'
+    else
+      @books = Book.all
+      @user = User.find(current_user.id)
+      render "index"
+    end
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = User.find(current_user.id)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = @book.user
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user #URLを入力しても画面に飛ばせない
+      render "edit"
+    else
+      redirect_to books_path
+    end
+  end
+    
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: 'You have update book successfully.'
+    else
+      render "edit"
+    end
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+    redirect_to books_path
+    end
+  end
+  
+end
